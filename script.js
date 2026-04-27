939bc3f83562fd1db2a0c5adff2cf74310bdf7f3
Abhinav Acharya <abhinavacharya707@gmail.com>
2026-04-27 15:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function isSameType(value1, value2) {
 	  if (typeof(value1)==NaN && typeof(value2) == NaN){
          return true;
 	  }
-    else if(typeof(value1)==typeof(value2)){
-	       return true;
+    else if(value1===value2){
+	       return true; 
 }
 else return false;
 }
